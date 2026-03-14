54d46cdc429f15eb6d672fced21990d556fecec8
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-14 17:38:30 +0530

    counter complete wuth useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 
 export default function Counter() {
-  const [decrement, setDecrement] = useState(0);
-  const [increment, setIncrement] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const incrementHandle = () => {
+    const updateCount = count + 1;
+    setCount(updateCount);
+  };
 
   const decrementHandle = () => {
-    const decrementUpdate = decrement + 1;
-    setDecrement(decrementUpdate);
+    const updateCount = count - 1;
+    setCount(updateCount);
   };
-  const incrementHandle = () => {
-    const incrementUpdate = increment - 1;
-    setDecrement(incrementUpdate);
+
+  const resetHandle = () => {
+    setCount(0);
   };
 
   return (
@@ -18,16 +22,20 @@ export default function Counter() {
       <h1>Counter </h1>
 
       <h2 style={{ fontSize: "40px" }} className="allUse">
-        Count : {decrement}
+        Count : {count < 0 ? "Wrong Number" : count}
       </h2>
+
+      <button onClick={incrementHandle} className="allUse">
+        Increment
+      </button>
 
       <button onClick={decrementHandle} className="allUse">
         Decrement
       </button>
-      
-      
-      <button onClick={incrementHandle} className="allUse">Increment</button>
-      <button className="allUse">Reset</button>
+
+      <button onClick={resetHandle} className="allUse">
+        Reset
+      </button>
     </div>
   );
 }
