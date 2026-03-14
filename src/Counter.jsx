362e917ce97eee80ad02b0efab125fcc74a0896e
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-14 16:24:16 +0530

    decrement and increment creat component

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 export default function Counter() {
+  const [decrement, setDecrement] = useState(0);
+  const [increment, setIncrement] = useState(0);
+
+  const decrementHandle = () => {
+    const decrementUpdate = decrement + 1;
+    setDecrement(decrementUpdate);
+  };
+  const incrementHandle = () => {
+    const incrementUpdate = increment - 1;
+    setDecrement(incrementUpdate);
+  };
+
   return (
     <div>
-      <p>Count </p>
+      <h1>Counter </h1>
+
+      <h2 style={{ fontSize: "40px" }} className="allUse">
+        Count : {decrement}
+      </h2>
+
+      <button onClick={decrementHandle} className="allUse">
+        Decrement
+      </button>
+      
+      
+      <button onClick={incrementHandle} className="allUse">Increment</button>
+      <button className="allUse">Reset</button>
     </div>
   );
 }
